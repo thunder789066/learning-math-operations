66a21bcd720b28d51951491a8e75a25fa586d59a
Christina Gerstner <christinalgerstner@gmail.com>
2020-04-07 18:29:23 -0500

    Initial commit

---
+++ Csharp.Lerning.MathOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Lerning.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5 + 3 =" + (5 + 3));
+            Console.WriteLine("5 - 3 =" + (5 - 3));
+            Console.WriteLine("5 * 3 =" + (5 * 3));
+            Console.WriteLine("5 / 3 =" + (5 / 3));
+            Console.WriteLine("5.2 % 3 =" + (5.2 % 3));
+
+            int i = 0;
+
+            Console.WriteLine("i++ =" + (i++));
+            Console.WriteLine("++i =" + (++i));
+            Console.WriteLine("i-- =" + (i--));
+            Console.WriteLine("--i =" + (--i));
+
+            Console.WriteLine("i += 3" + (i += 3));
+            Console.WriteLine("i -= 2" + (i -= 2));
+            Console.WriteLine("i *= 2" + (i *= 2));
+            Console.WriteLine("i /= 2" + (5 / 3));
+            Console.WriteLine("i %= 2" + (i %= 2));
+
+
+        }
+    }
+}
